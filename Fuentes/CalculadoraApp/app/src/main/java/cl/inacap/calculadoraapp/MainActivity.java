f76d717a6e912ee71b0eb258a0b1e340e156b79f
Jose Morales <joseemoralee@gmail.com>
2020-09-03 13:15:55 -0400

    generar calculadora
    ingresar icono
    ingrasar buton
    ingresar view

--- Fuentes/CalculadoraApp/app/src/main/java/cl/inacap/calculadoraapp/MainActivity.java
+++ Fuentes/CalculadoraApp/app/src/main/java/cl/inacap/calculadoraapp/MainActivity.java
@@ -3,17 +3,46 @@ package cl.inacap.calculadoraapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText n1Txt;
+    private EditText n2Txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.n1Txt = findViewById(R.id.n1Txt);
+        this.n2Txt = findViewById(R.id.n2Txt);
     }
 
-    public void saludar(view w){
-        Toast.makeText(this, "hola humano", Toast.LENGTH_LONG).show();
-    }
+   public void sumar(View v){
+        int n1 = Integer.parseInt(this.n1Txt.getText().toString());
+        int n2 = Integer.parseInt(this.n2Txt.getText().toString());
+       Toast.makeText(this, "la suma es"+ (n1 + n2), Toast.LENGTH_SHORT).show();
+
+   }
+
+   public void restar(View v){
+       int n1 = Integer.parseInt(this.n1Txt.getText().toString());
+       int n2 = Integer.parseInt(this.n2Txt.getText().toString());
+       Toast.makeText(this, "la resta es"+ (n1 - n2), Toast.LENGTH_SHORT).show();
+   }
+
+   public void miltiplicar(View v){
+
+       int n1 = Integer.parseInt(this.n1Txt.getText().toString());
+       int n2 = Integer.parseInt(this.n2Txt.getText().toString());
+       Toast.makeText(this, "la multiplicacion es es"+ (n1 * n2), Toast.LENGTH_SHORT).show();
+   }
+
+   public void dividir(View v){
+       int n1 = Integer.parseInt(this.n1Txt.getText().toString());
+       int n2 = Integer.parseInt(this.n2Txt.getText().toString());
+       Toast.makeText(this, "la division es"+ (n1 / n2), Toast.LENGTH_SHORT).show();
+
+   }
 }
